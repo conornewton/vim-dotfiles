c21e0b95ef1f4bace1687dfbd9ef1a582d285e8d
Conor Newton <conornewton@gmail.com>
2022-01-07 16:19:12 +0000

    fixed luasnip bindings

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -3,7 +3,9 @@ require("luasnip/loaders/from_vscode").load({
 	paths = { "~/.local/share/nvim/site/pack/packer/start/friendly-snippets" },
 })
 
+-- lua snip shortcuts
 vim.cmd([[
   imap <silent><expr> <c-j> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Plug>(Tabout)'
   imap <silent><expr> <c-k> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<Plug>(TaboutBack)'
+  imap <c-l> <Plug>luasnip-expand-snippet
 ]])
