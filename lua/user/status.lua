a3bf13468eec8f94c030136201719121810a16d1
Conor Newton <conornewton@gmail.com>
2022-06-10 17:32:17 +0100

    some statusline improvements

--- lua/user/status.lua
+++ lua/user/status.lua
@@ -46,16 +46,44 @@ local function mode()
 	return string.format(" %s ", modes[current_mode]):upper()
 end
 
-function _G.StatusLine()
-	return table.concat({
-		"%#Statusline#",
-		update_mode_colors(),
-		mode(),
-		"%#StatusLine#",
-		"%{get(b:,'gitsigns_status','')}",
-		" %f",
-	})
+local function isGit()
+	return vim.fn.systemlist("git rev-parse --is-inside-work-tree")[1] == "true"
 end
+
+-- TODO disable git information
+local function git_branch()
+	-- local git = require("nvim-web-devicons").get_icon("branch")
+	return " " .. vim.fn.systemlist("git branch --show-current")[1]
+end
+
+function _G.StatusLine()
+	if isGit() then
+		return table.concat({
+			"%#Statusline#",
+			update_mode_colors(),
+			mode(),
+			"%#StatusLine#",
+			" ",
+			git_branch(),
+			" ",
+			"%{get(b:,'gitsigns_status','')}",
+			" %f",
+		})
+	else
+		return table.concat({
+			"%#Statusline#",
+			update_mode_colors(),
+			mode(),
+			"%#StatusLine#",
+			" ",
+			" %f",
+		})
+	end
+end
+
+--TODO: global diagnostics in statusline
+--TODO: colourscheme stuff
+--TODO: have a build a component function.
 
 vim.api.nvim_set_hl(0, "StatuslineInsertAccent", { fg = "Black", bg = "White" })
 vim.api.nvim_set_hl(0, "StatuslineVisualAccent", { fg = "Black", bg = "Orange" })
